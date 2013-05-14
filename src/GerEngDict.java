8e4bc34d70796f5136f1c701c773a5eff4ae49b0
tomsn <t.schwarzmayr@gmail.com>
2013-05-14 21:39:32 +0200

    a little testing

--- src/GerEngDict.java
+++ src/GerEngDict.java
@@ -1,15 +1,17 @@
 package src;
 
+import org.bson.BSONObject;
 import org.bson.types.ObjectId;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 import com.mongodb.WriteResult;
+import com.sun.corba.se.spi.legacy.connection.GetEndPointInfoAgainException;
 
 public class GerEngDict extends DictManager {
 	private DBCollection gerTable = null;
 	private DBCollection engTable = null;
-	private DBCollection eng_ger_translation = null;
+	private DBCollection engGerTranslationTable = null;
 	
 	public GerEngDict() {
 		super();
@@ -19,24 +21,5 @@ public class GerEngDict extends DictManager {
 		this.eng_ger_translation = this.dictDB.getDBCollection(Constants.ENG_GER_TRANSLATION_TABLE_NAME);
 	}
 	
-	private int insertWordTupel(String gerWord, String gerExample, 
-								String engWord, String engExample) {
-		WriteResult wr = null;
-		BasicDBObject gerEntry = new BasicDBObject();
-		gerEntry.put("word", gerWord);
-		gerEntry.put("example", gerExample);
-		wr = this.gerTable.insert(gerEntry);
-		ObjectId gerEntryOID = (ObjectId)gerEntry.get( "_id" );
-		
-		BasicDBObject engEntry = new BasicDBObject();
-		engEntry.put("word", "table");
-		engEntry.put("example", "");
-		engTable.insert(engEntry);
-		ObjectId engEntryOID = (ObjectId)engEntry.get("_id");
-		
-		BasicDBObject engGerTranslEntry = new BasicDBObject();
-		engGerTranslEntry.put("gerID", gerEntryOID);
-		engGerTranslEntry.put("engID", engEntryOID);
-		eng_ger_translation.insert(engGerTranslEntry);
-	}
+	
 }
